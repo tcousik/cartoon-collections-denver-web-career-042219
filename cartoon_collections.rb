535c000bd4e6286e115a826737782ae8eefdf1c5
Tejas Cousik <tcousik0@gmail.com>
2019-04-21 19:21:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(list)
   array = []
   list.each_with_index.do |name, index|
-    puts 
+    array.push ("#{name + 1}. #{index}")
 end
 
 def summon_captain_planet# code an argument here
