253140d364ce72b0a96513311969f5d8ab6dcb71
Tejas Cousik <tcousik0@gmail.com>
2019-04-21 19:46:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do (cheese)
+  array.find do |cheese|
     cheese_types.include?(cheese)
   end
 end
